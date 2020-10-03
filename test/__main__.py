27a70be696bd3c023db8c6f9b1db44c071fd47c3
Egg-Fish <eugeniomanansala@gmail.com>
2020-10-03 23:31:29 +0800

    idk

--- test/__main__.py
+++ test/__main__.py
@@ -3,5 +3,3 @@ from sonrisa.test.test_formatting import test_formatting
 import sonrisa.functions.formatting as f
 
 test_formatting()
-
-#print(f.separate_categories("18/7/20, 4:41 pm - egg: You deleted this message"))
