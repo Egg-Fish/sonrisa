39bcb2d8db0258109d468f81be0ac73fccfabccf
Egg-Fish <eugeniomanansala@gmail.com>
2020-10-05 15:50:52 +0800

    started processing

--- __main__.py
+++ __main__.py
@@ -1,7 +1,11 @@
 from sonrisa.functions import formatting, processing
+import matplotlib.pyplot as plt
+import numpy as np
 
 path = "C:\\Users\\Egie\\Desktop\\Projects\\SonrisaProjects\\Text\\Amar.txt"
 
 data = formatting.ChatData(path)
 
-print(data.search(year="2019"))
+freq = processing.message_frequency(data, 20)
+
+print(freq)
--- functions/processing.py
+++ functions/processing.py
@@ -1,1 +1,28 @@
-print("Hello, world")
+from collections import Counter
+import pandas as pd
+import numpy as np
+
+def message_frequency(df, n = 0, message = None):
+    data = df.data["Message"].values
+    words= []
+
+    for message in data:
+        words.extend([word for word in message.split()])
+
+    data = pd.Series(words).value_counts()
+
+    if n != 0 and message != None:
+        raise Exception("n and message cannot co-exist")
+
+    if n != 0:
+        data = data.iloc[:n]
+
+    if message != None:
+        if type(message) == list:
+            data = data.loc[np.isin(data.index, message)]
+
+        else:
+            data = data.loc[data.index == message]
+
+    return data
+
