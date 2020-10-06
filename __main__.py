bc886c0cd1564c97b1e3ffc3581ec6368ced9fec
Egg-Fish <eugeniomanansala@gmail.com>
2020-10-06 21:58:07 +0800

    random lol

--- __main__.py
+++ __main__.py
@@ -2,9 +2,11 @@ from sonrisa.functions import formatting, processing
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = "C:\\Users\\Egie\\Desktop\\Projects\\SonrisaProjects\\Text\\GC1.txt"
+path = "C:\\Users\\Egie\\Desktop\\Projects\\SonrisaProjects\\Text\\C1.txt"
 
 data = formatting.ChatData(path)
 
-print(data.search(date="18/2/19",year="2019"))
-
+q = processing.message_frequency(data.data, message = ["simp","lol"])
+print(q)
+q.plot("pie")
+plt.show()
